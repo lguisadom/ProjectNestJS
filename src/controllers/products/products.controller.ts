da6c580c0f28f8e19b50a4773c4532b70c018689
Luis Guisado <luis.guisado.matrix@gmail.com>
2022-04-20 18:17:02 -0500

    Handling POST HTTP requests

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ppid } from 'process';
+import { ProductRequest } from './product.request';
 
 @Controller('products')
 export class ProductsController {
@@ -33,4 +35,32 @@ export class ProductsController {
         return "1, 2, 3, 4, 5";
     }
 
+    @Post()
+    createPost(): string {
+        return `Estamos atendiendo una solicitud de tipo POST`;
+    }
+
+    @Post("/create")
+    createProduct(@Body() body): string {
+        return `Nombre: ${body.name} y Descripcion: ${body.description}`;
+    }
+
+    @Post("/createv2")
+    createProductv2(
+        @Body("name") name: string,
+        @Body("description") description: string
+    ): string {
+        return `Nombre: ${name} y Descripcion: ${description}`;
+    }
+
+    @Post("/createv3")
+    createProductv3(
+        @Body() body: ProductRequest
+    ): string {
+        if ((body as any).isError) {
+            return 'Error';
+        }
+
+        return `Nombre: ${body.name} y Descripcion: ${body.description}`;
+    }
 }
