f3af9ed2c7cabbe0817261a7177ebef6c333b9e5
Luis Guisado <lguisadom@gmail.com>
2022-10-31 01:59:39 -0500

    Update

--- makefiles/container.mk
+++ makefiles/container.mk
@@ -1,8 +1,11 @@
-CONTAINER_NAME	?= ProjectNestJS
-IMAGE_BUILD			?= lguisadom/project-nestjs:1.0
-PORT						?= 3000
-IMAGE_BASE			?= node:16.17.1-alpine3.16
+UID_LOCAL				?= $(shell id -u)
+GID_LOCAL				?= $(shell id -g)
+CONTAINER_NAME	= ProjectNestJS
+IMAGE_BUILD			= lguisadom/project-nestjs:1.0
+PORT						= 3000
+IMAGE_BASE			= node:16.17.1-alpine3.16
+APP_DIR					= app
 
 ## Levanta en contenedor docker
 up-docker:
-		docker container run -p "$(PORT):$(PORT)" --rm "$(IMAGE_BUILD)"
+	docker container run -p "$(PORT):$(PORT)" --rm "$(IMAGE_BUILD)"
